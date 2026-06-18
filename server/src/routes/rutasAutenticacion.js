6e2185e37c89ea09151df91bb12dba308fdbc108
carlitosvargas <carloscabj@gmail.com>
2026-06-18 12:13:58 -0300

    aislar codigo del cliente

--- server/src/routes/rutasAutenticacion.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const enrutador = express.Router();
-const { iniciarSesion, registrarUsuario } = require('../controllers/controladorAutenticacion');
-
-// Definir rutas
-enrutador.post('/iniciar-sesion', iniciarSesion);
-enrutador.post('/registro', registrarUsuario);
-
-module.exports = enrutador;
